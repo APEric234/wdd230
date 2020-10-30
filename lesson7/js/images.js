543ef8006e7e3acad83bbb4e3571f2cfc308c145
Eric Allen <aperic234@gmail.com>
2020-10-29 19:09:00 -0600

    added timer to slow it down

--- lesson7/js/images.js
+++ lesson7/js/images.js
@@ -11,7 +11,10 @@ function preloadImage (img){
   img.classList.add(class_set);
 }
 
-const imgOptions = {};
+const imgOptions = {
+  threshold: 1,
+  rootmargin:"0px 0px 50px 0px"
+};
 const imgObserver = new IntersectionObserver((entries,imgObserver) => {
 entries.forEach(entry => {
   if(!entry.isIntersecting){
