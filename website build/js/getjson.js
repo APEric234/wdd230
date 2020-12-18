1997fe35dc5cb7ba409383072b56eade60519168
Eric Allen <aperic234@gmail.com>
2020-12-17 19:31:30 -0700

    sent changes

--- website build/js/getjson.js
+++ website build/js/getjson.js
@@ -1,22 +1,26 @@
-function temp(proph) {
-  let card=document.createElement('section');
-  let title=document.createElement('h2');
-  title.textContent=proph.name + ' ' + proph.lastname;
-
-  let subtitle1 = document.createElement('h3');
-  subtitle1.textContent='Date of Birth:' +proph.birthdate;
-  let subtitle2 = document.createElement('h3');
-  subtitle2.textContent='Place of Birth:' +proph.birthplace;
-
+function temp(car) {
+  let card=document.createElement('tr');
+  let name=document.createElement('td');
+  name.textContent=car.name
+  let reserv_h = document.createElement('td');
+  let resererv_f = document.createElement('td');
+  let walk_h = document.createElement('td');
+  let walk_f = document.createElement('td');
+  reserv_h = car.half_r;
+  resererv_f = car.full_r;
+  walk_f = car.full;
+  walk_h = car.half;
+  let image_td=document.createElement(td);
   let image = document.createElement('img');
-  image.setAttribute('src', proph.imageurl);
-  image.setAttribute('alt', proph.name + ' ' + proph.lastname + ' - '+proph.order);
-  card.appendChild(title);
-  card.appendChild(subtitle1);
-  card.appendChild(subtitle2);
-  card.appendChild(image);
+  image.setAttribute('src', "/images/"+car.img);
+  image.setAttribute('alt', car.name);
+  card.appendChild(reserv_h);
+  card.appendChild(resererv_f);
+  card.appendChild(walk_h);
+  image_td.appendChild(image);
+  card.appendChild(image_td);
 
-  document.querySelector('div.cards').appendChild(card);
+  document.querySelector('table#rentals').appendChild(card);
 }
 
 const requestURL = 'json/stuff.json';
@@ -26,10 +30,10 @@ fetch(requestURL).then(function (response){
 .then(
   function ( jsonObject){
     console.table(jsonObject);
-    const prophets = jsonObject['prophets'];
-    for (let prophet in prophets){
-      console.table(prophet);
-      /*temp(prophets[prophet]);*/
+    const vehicles = jsonObject['vehihcles'];
+    for (let vehic in vehicles){
+      console.table(vehic);
+      temp(vehicles[vehic]);
     }
   }
 )
